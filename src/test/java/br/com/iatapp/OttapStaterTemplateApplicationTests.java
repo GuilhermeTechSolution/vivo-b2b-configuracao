8f9eaceaa8b4e634bf27664e38daca94a5bb4ccb
Guilherme Gonzaga Soares <guilherme.gonzaga@techsolution.com>
2026-01-15 18:42:32 -0300

    chore: configura teste

--- src/test/java/br/com/iatapp/OttapStaterTemplateApplicationTests.java
+++ src/test/java/br/com/iatapp/OttapStaterTemplateApplicationTests.java
@@ -6,7 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
+				+ "org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration,"
+				+ "org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration" })
 public class OttapStaterTemplateApplicationTests {
 
 	@Test
