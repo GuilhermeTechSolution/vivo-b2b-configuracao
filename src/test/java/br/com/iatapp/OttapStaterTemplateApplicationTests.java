7bb7c24b819977323e22762431810341f83ba03b
Guilherme Gonzaga Soares <guilherme.gonzaga@techsolution.com>
2026-01-15 18:47:08 -0300

    chore: configura teste

--- src/test/java/br/com/iatapp/OttapStaterTemplateApplicationTests.java
+++ src/test/java/br/com/iatapp/OttapStaterTemplateApplicationTests.java
@@ -3,6 +3,8 @@ package br.com.iatapp;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -11,6 +13,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 				+ "org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration,"
 				+ "org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration" })
 public class OttapStaterTemplateApplicationTests {
+
+	@MockBean
+	private MongoTemplate mongoTemplate;
 
 	@Test
 	public void contextLoads() {
